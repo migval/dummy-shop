c0ca6b2995e255676b3c77d4963e8ff16231a046
Miguel Valiente <mig.valiente@gmail.com>
2024-01-07 17:45:56 +0100

    feat: show products list in dashboard

--- src/app/products/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/products/pages/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ProductsService } from "../../services/products.service";
+import { CustomProductDto } from "../../services/dtos/product.dto";
 
 @Component({
     selector: 'app-dashboard-page',
     templateUrl: './dashboard-page.component.html',
     styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent {
-    constructor() { }
+export class DashboardPageComponent implements OnInit {
+
+    public products: CustomProductDto[] = [];
+
+    constructor(private productsSrv: ProductsService) { }
+
+    ngOnInit() {
+        this.productsSrv.fetchProducts<CustomProductDto>(0, 10, ['thumbnail', 'title', 'description', 'price'])
+        .subscribe((response) => {
+            this.products = response.products;
+        });
+    }
 }
