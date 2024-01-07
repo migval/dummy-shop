0180999ea222cc2aa7249aafd5b9b091521ab743
Miguel Valiente <mig.valiente@gmail.com>
2024-01-07 15:23:48 +0100

    feat: auth service

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable, tap } from "rxjs";
+import { environment } from "src/environments/environment";
+import { UserDto } from "./dtos/user.dto";
+import { UserService } from "./user.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+
+    constructor(private http: HttpClient, private userService: UserService) { }
+
+    login(username: string, password: string): Observable<UserDto> {
+        return this.http.post<UserDto>(`${environment.apiUrl}/auth/login`, { username, password }).pipe(
+            tap(user => this.userService.setUser(user))
+        );
+    }
+}
